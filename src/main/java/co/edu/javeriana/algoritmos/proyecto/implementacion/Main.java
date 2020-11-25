489e6f43dc01dfd927e6730fcae214251e91542f
Yotas01 <62121339+Yotas01@users.noreply.github.com>
2020-11-25 18:02:54 -0500

    Update Main.java
    Final

--- src/main/java/co/edu/javeriana/algoritmos/proyecto/implementacion/Main.java
+++ src/main/java/co/edu/javeriana/algoritmos/proyecto/implementacion/Main.java
@@ -1,45 +1,45 @@
 package co.edu.javeriana.algoritmos.proyecto.implementacion;
 
 import co.edu.javeriana.algoritmos.proyecto.Casilla;
+import co.edu.javeriana.algoritmos.proyecto.Jugador;
+import co.edu.javeriana.algoritmos.proyecto.Tablero;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
-        // Crear tablero aleatorio
-        // Imprimir tablero
-        // jugadorSolucion.jugar()
-        // Imprimir Lista de casillas
-
-        // Pruebas
-        /*
-         * Prueba para casillaValida() -> test pasados
-         */
-        /*
-         * TableroJuego tablero = new TableroJuego(); for (int i = 0; i < 4; i++) { for
-         * (int j = 0; j < 5; j++) { System.out.print(tablero.casillaValida(new
-         * Casilla(i,j)) + " "); } System.out.println(); }
-         */
-
-        /**
-         * Prueba para vecinosCasilla()
-         */
+    public static void main(String[] args) throws CloneNotSupportedException {
+        
         TableroJuego tablero = new TableroJuego();
-        /*
-         * for (int i = 0; i < 4; i++) { for (int j = 0; j < 5; j++) {
-         * System.out.print(tablero.vecinosCasilla(new Casilla(i, j)) + " "); }
-         * System.out.println(); }
-         */
-        /**
-         * Prueba para efecuarJugada()
-         */
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 5; j++) {
-                tablero.imprimirTablero();
-                System.out.print(tablero.efectuarJugada(new Casilla(i, j)) + " ");
-                tablero.imprimirTablero();
+        Tablero aux = new TableroJuego(tablero.getFilas(),tablero.getColumnas(),tablero.getNumeroColores());
+        for (int i = 0; i < aux.getFilas(); i++) {
+            for (int j = 0; j < aux.getColumnas(); j++) {
+                aux.coloresTablero()[i][j] = tablero.coloresTablero()[i][j];
             }
-            System.out.println();
         }
-
+        Jugador nosotros = new JugadorSolucion();
+        List<Casilla> jugadas = nosotros.jugar(aux);
+        int puntaje = 0;
+        
+        /*for (int i = 0; i < tablero.getFilas(); i++) {
+            for (int j = 0; j < tablero.getColumnas(); j++) {
+                System.out.print(tablero.coloresTablero()[i][j] + " ");
+            }
+            System.out.println("");
+        }
+        System.out.println("\n\n");
+        for (int i = 0; i < tablero.getFilas(); i++) {
+            for (int j = 0; j < tablero.getColumnas(); j++) {
+                System.out.print(aux.coloresTablero()[i][j] + " ");
+            }
+            System.out.println("");
+        }*/
+        
+        
+        for (Casilla jugada : jugadas) {
+            //System.out.println("Jugada:" + jugada.toString());
+            puntaje += tablero.efectuarJugada(jugada);
+        }
+        //tablero.imprimirTablero();
+        System.out.println("Puntaje final: " + puntaje);
     }
 }
